5436ce9eb4db0bf87d796ded8bb06ded5af19ff6
me-cafebabe <me.cafebabe@gmail.com>
2023-08-30 17:11:44 +0800

    Update project structure & Convert libGLESv2_adreno.so and libC2D2.so to build module

--- mithorium-common-graphics-vendor.mk
+++ mithorium-common-graphics-vendor.mk
@@ -1,12 +1,9 @@
 ifeq ($(TARGET_USES_Q_DISPLAY_STACK),true)
     ifeq ($(TARGET_BOARD_PLATFORM),msm8937)
-        PRODUCT_COPY_FILES += \
-            $(call find-copy-subdir-files,*,vendor/xiaomi/mithorium-common-graphics/olive-Q/vendor/,$(TARGET_COPY_OUT_VENDOR)/)
+        $(call inherit-product, vendor/xiaomi/mithorium-common-graphics/olive-Q/extract-vendor.mk)
     else ifeq ($(TARGET_BOARD_PLATFORM),msm8953)
-        PRODUCT_COPY_FILES += \
-            $(call find-copy-subdir-files,*,vendor/xiaomi/mithorium-common-graphics/onc-Q/vendor/,$(TARGET_COPY_OUT_VENDOR)/)
+        $(call inherit-product, vendor/xiaomi/mithorium-common-graphics/onc-Q/extract-vendor.mk)
     endif
 else
-    PRODUCT_COPY_FILES += \
-        $(call find-copy-subdir-files,*,vendor/xiaomi/mithorium-common-graphics/LA.UM.9.6.3.r1-06200-89xx.0/vendor/,$(TARGET_COPY_OUT_VENDOR)/)
+    $(call inherit-product, vendor/xiaomi/mithorium-common-graphics/LA.UM.9.6.3.r1-06200-89xx.0/extract-vendor.mk)
 endif
